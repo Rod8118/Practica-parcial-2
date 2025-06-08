e22ebb47f7d392caca7856ed6a975ea622c88c94
Kyowxx <fabian.rodriguez@utp.ac.pa>
2025-06-07 20:23:58 -0500

    Add files via upload

---
+++ practicexd/src/Prestamos/Cliente.java
@@ -0,0 +1,19 @@
+package Prestamos;
+
+public class Cliente {
+    String nombre;
+    char sexo;
+    double salario;
+    double prestamo;
+    boolean aprobado;
+
+     public Cliente(String nombre, char sexo, double salario, double prestamo){
+        this.nombre=nombre;
+        this.sexo=sexo;
+        this.salario=salario;
+        this.prestamo=prestamo;
+        this.aprobado=aprobado;
+    }
+
+
+}
---
+++ practicexd/src/Prestamos/Operador.java
@@ -0,0 +1,19 @@
+package Prestamos;
+
+public class Operador {
+    int clientesTram;
+    int clientesAprob;
+    int hombresAprob;
+    int mujeresAprob;
+    double totalPres;
+
+    public Operador(){
+        this.clientesTram=clientesTram;
+        this.clientesAprob=clientesAprob;
+        this.hombresAprob=hombresAprob;
+        this.mujeresAprob=mujeresAprob;
+        this.totalPres=totalPres;
+    }
+
+
+}
